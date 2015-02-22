cbcd85e7bb7a769aaae2de3ee242c7271538ebcb
16robbertza <16robbertza@abschools.org>
2015-02-22 18:27:42 -0500

    Almost Done

--- Display.java
+++ Display.java
@@ -1,95 +1,58 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Line2D;
-
 import javax.swing.*;
 
-
+/*
+ * So, I got the line to rotate and stuff, but I haven't done any extra stuff. 
+ * I don't think well really have time. 
+ * You can just take the rotate() method if you want, but I can change my stuff too if you've done it a better way. 
+ * All I would have to do is just change the parameters. 
+ * 
+ * Best of luck, 
+ * Andrew
+ */
 public class Display extends JFrame {
 
-	JFrame frame = new JFrame("Rotating Lines");
-	JPanel buttonPanel = new JPanel(new GridBagLayout());
-	JPanel linePanel = new JPanel(new BorderLayout());
-	LineComponent lineComponent = new LineComponent(frame.getContentPane().getWidth(), frame.getContentPane().getHeight());
-	Timer timer = new Timer(100, new ActionListener(){
-		public void actionPerformed(ActionEvent e){
-			lineComponent.repaint();
+	private JFrame frame = new JFrame("Rotating Lines");
 
-			//			Line2D.Double line = new Line2D.Double(
-			//					0,0,
-			//					frame.getContentPane().getWidth(), frame.getContentPane().getHeight() -50
-			//					);
-			//			
-			//			lineComponent.line = line;
-			//			
-			//			frame.getContentPane().add(lineComponent);
+	public Display() {
 
-		}
-	});
-
-	public Display(){
-
-		frame.setLayout(new GridBagLayout());
+		frame.setLayout(new BorderLayout());
 		frame.setSize(500, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
 		frame.setLayout(new BorderLayout());
 		frame.setVisible(true);
 
+		LineComponent lineComponent = new LineComponent(frame);
+		frame.getContentPane().add(lineComponent, BorderLayout.CENTER);
 
-		buttonPanel.setSize(500 ,50);
+		JPanel buttonPanel = new JPanel(new GridBagLayout());
+		buttonPanel.setSize(500, 50);
 		buttonPanel.setBackground(Color.GRAY);
 		frame.getContentPane().add(buttonPanel, BorderLayout.NORTH);
 
-
 		GridBagConstraints c = new GridBagConstraints();
-
 		c.anchor = GridBagConstraints.CENTER;
-
 		c.insets = new Insets(10, 10, 10, 10);
-		JButton start = new JButton("Start");
+
+		JButton start = new JButton("Start / Stop");
 		c.gridx = 0;
 		c.gridy = 0;
-		start.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				timer.start();
-
-				Line2D.Double line = new Line2D.Double(
-						0,0,
-						frame.getContentPane().getWidth(), frame.getContentPane().getHeight() -50
-						);
-
-				lineComponent.line = line;
-
-				frame.getContentPane().add(lineComponent);
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (lineComponent.getStartStop() == 0) {
+					lineComponent.setStartStop(1);
+					lineComponent.setTimer(true);
+				} else {
+					lineComponent.setStartStop(0);
+					lineComponent.setTimer(false);
+				}
+				;
 			}
 		});
-		buttonPanel.add(start, c);		
-
-		JButton stop = new JButton("Stop");
-		c.gridx = 1;
-		c.gridy = 0;
-		buttonPanel.add(stop, c);
-		stop.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				timer.stop();
-			}
-		});
-
-		JSlider speed = new JSlider();
-		c.gridx =  2;
-		c.gridy = 0;
-		buttonPanel.add(speed);
-
-
-//		Line2D.Double line = new Line2D.Double(
-//				0,0,
-//				frame.getContentPane().getWidth(), frame.getContentPane().getHeight() -50
-//				);
-//
-//		lineComponent.line = line;
-//
-//		frame.getContentPane().add(lineComponent);
+		buttonPanel.add(start, c);
 
 		frame.setVisible(true);
 
@@ -98,26 +61,6 @@ public class Display extends JFrame {
 	public static void main(String[] args) {
 
 		new Display();
-
-		//		Display display = new Display();
-		//		
-		//		Line2D.Double line = new Line2D.Double(
-		//				0,0,
-		//				display.frame.getContentPane().getWidth(), display.frame.getContentPane().getHeight() -50
-		//				);
-		//		
-		//		display.lineComponent.line = line;
-		//		
-		//		display.frame.getContentPane().add(display.lineComponent);
-		//		
-		//		display.repaint();
-
-
-
-
-
-
-
 
 	}
 
--- LineComponent.java
+++ LineComponent.java
@@ -1,38 +1,110 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Line2D;
 import java.util.*;
-
 import javax.swing.*;
-
+import javax.swing.Timer;
 
 public class LineComponent extends JComponent {
 
+	private JFrame frame;
+	private Line2D.Double line;
+	double[][] points = new double[2][2];
+	double elapsedTime;
+	private int startStop;
 
-	Line2D.Double line;
-	
-	LineComponent(int width, int height) {
+	private ActionListener rotation = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			// System.out.println(lineToString(line));
+
+			elapsedTime += 1.5;
+
+			// points = rotate(points);
+			points = rotate(points, elapsedTime, frame);
+			line.setLine(points[0][0], points[0][1], points[1][0], points[1][1]);
+
+			LineComponent.this.repaint();
+		}
+	};
+
+	private Timer timer = new Timer(100, rotation);
+
+	LineComponent(JFrame frame) {
 		super();
-		setPreferredSize(new Dimension(width,height));
-		line = new Line2D.Double();
 		
-	}
+		this.frame = frame;
+		this.elapsedTime = 0;
+		this.startStop = 0;
 
+		points[0][0] = 0;
+		points[0][1] = 0;
+		points[1][0] = 0;
+		points[1][1] = 0;
+
+		line = new Line2D.Double(points[0][0], points[0][1], points[1][0],
+				points[1][1]);
+
+	}
 
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		Dimension d = getPreferredSize();
-		g.setColor(Color.black);
-			g.drawLine(
-					(int)line.getX1(),
-					(int)line.getY1(),
-					(int)line.getX2(),
-					(int)line.getY2()
-					);
-		
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.black);
+		g2.draw(line);
 	}
 
+	public static double[][] rotate(double[][] points) {
+		points[0][0] = 0;
+		points[0][1] = 0;
+		points[1][0] += 20;
+		points[1][1] += 20;
+
+		return points;
+	}
+
+	public static double[][] rotate(double[][] points, double timeElapsed,
+			JFrame frame) {
+		double theta = timeElapsed / 57.3;
+		// System.out.println("Theta" + theta);
+		double r = 0;
+		double a = frame.getWidth();
+		// System.out.println("Width" + a);
+		double b = frame.getHeight();
+		// System.out.println("Height" + b);
+		if (a >= b) {
+			r = frame.getWidth() / Math.sqrt(2) + 1;
+		}
+		if (a < b) {
+			r = frame.getHeight() / Math.sqrt(2) + 1;
+		}
+		// System.out.println("Radius" + r);
+
+		points[0][0] = r * Math.cos(theta) + frame.getWidth() / 2;
+		points[0][1] = r * Math.sin(theta) + frame.getContentPane().getHeight() / 2 - 15;
+		points[1][0] = r * Math.cos(theta + Math.PI) + frame.getWidth() / 2;
+		points[1][1] = r * Math.sin(theta + Math.PI)  + frame.getContentPane().getHeight() / 2 - 15;
+
+		return points;
+	}
+	
+	public static String lineToString(Line2D.Double line) {
+		return "(" + line.getX1() + "," + line.getY1() + ")   (" + line.getX2() + "," + line.getY2() + ")";
+	}
+	
+	public void setTimer(boolean on) {
+		if (on)
+			timer.start();
+		else
+			timer.stop();
+	}
+
+	public int getStartStop() {
+		return startStop;
+	}
+
+	public void setStartStop(int startStopSwitch) {
+		this.startStop = startStopSwitch;
+	}
 
 }
-
 
