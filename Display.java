f75abccf7e97bdf2fe7bc30cf19af2fa8b544beb
16robbertza <16robbertza@abschools.org>
2015-02-28 12:53:49 -0500

    finished comments

--- Display.java
+++ Display.java
@@ -2,18 +2,34 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/*
+ * Amy Liu and Andrew Robbertz - Period 5 - Last Updated 2/28/15
+ * 
+ * public Class Display is used to create the visible window used show a rotating line. 
+ */
 public class Display extends JFrame {
 
-	private JFrame frame = new JFrame("Rotating Lines");
-
+	
+	/*
+	 * Constructor fore creating Display objects.
+	 * Creates a JFrame, sets it to be 500X550 pixels, and resizable. 
+	 * 
+	 * Creates a LineComponent object to be able to be painted on to the Display. 
+	 * 
+	 * Creates a button at the top of the Display that can start and stop the rotation of the LineComponent.
+	 *  Upon the first click, the line will start to rotate, and on the second it will stop rotating. 
+	 *  
+	 */
 	public Display() {
 
+		JFrame frame = new JFrame("Rotating Lines");
 		frame.setLayout(new BorderLayout());
 		frame.setSize(500, 550);
+		frame.setMinimumSize(new Dimension(500, 550));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
 		frame.setLayout(new BorderLayout());
-		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 
 		LineComponent lineComponent = new LineComponent(frame);
 		frame.getContentPane().add(lineComponent, BorderLayout.CENTER);
@@ -32,10 +48,10 @@ public class Display extends JFrame {
 		c.gridy = 0;
 		start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (!lineComponent.getStartStop()) {
-					lineComponent.setStartStop(true);
+				if (!lineComponent.getRotate()) {
+					lineComponent.setRotate(true);
 				} else {
-					lineComponent.setStartStop(false);
+					lineComponent.setRotate(false);
 				};
 			}
 		});
@@ -45,6 +61,7 @@ public class Display extends JFrame {
 
 	}
 
+	//Main method to call constructor for Display. 
 	public static void main(String[] args) {
 
 		new Display();
--- LineComponent.java
+++ LineComponent.java
@@ -5,31 +5,50 @@ import java.awt.geom.Line2D;
 import javax.swing.*;
 import javax.swing.Timer;
 
+/* 
+ * Amy Liu and Andrew Robbertz - Period 5 - Last Updated 2/28/15
+ * 
+ * Class LineComponent used to paint 2D lines. 
+ * 
+ * Private Data:
+ * JFrame frame: the frame that the line is to be painted on. 
+ * double[] points: the two endpoints of the line, so that they may be accessed in the paintComponent(). 
+ * boolean rotate: determines whether or not to rotate the line when updating it on the screen.
+ * Line1 ln: the line object that is created, so that the endpoints of the line can be manipulated, and rotated. 
+ * Timer timer: used to repaint the line on the display every tenth of a second. 
+ */
 public class LineComponent extends JComponent {
 
 	private JFrame frame;
 	private double[][] points = new double[2][2];
-	private boolean startStop;
+	private boolean rotate;
 	private Line1 ln;
-	
-	private ActionListener rotation = new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			//System.out.println(lineToString(line));
 
-			if(startStop) ln.rotateOnce(0);			
+	private Timer timer = new Timer(100, new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if(rotate) ln.rotateOnce(0);			
 			points = ln.getPoints(frame.getContentPane().getWidth(), frame.getContentPane().getHeight() - 50);
-			
 			LineComponent.this.repaint();
 		}
-	};
+	});
 
-	private Timer timer = new Timer(100, rotation);
-	
+	/*
+	 * Constructor for creating LineComnponent objects.
+	 * 
+	 * Parameters:
+	 * Jframe frame: the frame that the line is to be painted on. 
+	 * 
+	 * Initializes the endpoints to be (0, 0) and (0, 0).
+	 * Initializes boolean rotate to false, so they user has to start the rotation of the line.
+	 * Initializes Line1 ln to a new Line1 using frame, and a drawAngle of 0. 
+	 * Starts the timer to repaint and update the line. 
+	 * 
+	 */
 	LineComponent(JFrame frame) {
 		super();
 		
 		this.frame = frame;
-		this.startStop = false;
+		this.rotate = false;
 
 		points[0][0] = 0;
 		points[0][1] = 0;
@@ -42,6 +61,15 @@ public class LineComponent extends JComponent {
 		
 	}
 
+	/*
+	 * Method used to paint the line onto the ContentPane. 
+	 * paints the line given the endpoins. 
+	 * Sets the stroke to 3 and the color to be Color.BLACK.
+	 * 
+	 * Parameters: 
+	 * Graphics g 
+	 * 
+	 */
 	public void paintComponent(Graphics g) {
 		Line2D.Double line = new Line2D.Double(points[0][0], points[0][1], points[1][0], points[1][1]);
 		Graphics2D g2 = (Graphics2D) g;
@@ -49,24 +77,25 @@ public class LineComponent extends JComponent {
 		g2.setStroke(new BasicStroke(3));
 		g2.draw(line);
 	}
-	
-	public static String lineToString(Line2D.Double line) {
-		return "(" + line.getX1() + "," + line.getY1() + ")   (" + line.getX2() + "," + line.getY2() + ")";
-	}
-	
-	public void setTimer(boolean on) {
-		if (on)
-			timer.start();
-		else
-			timer.stop();
+
+	/*
+	 * Getter for private boolean rotate.
+	 * 
+	 * Returns: 
+	 * boolean rotate: whether the line is rotating or not. 
+	 */
+	public boolean getRotate() {
+		return rotate;
 	}
 
-	public boolean getStartStop() {
-		return startStop;
-	}
-
-	public void setStartStop(boolean startStop) {
-		this.startStop = startStop;
+	/*
+	 * Setter for private boolean rotate. 
+	 * 
+	 * Parameters:
+	 * boolean rotate: true if you wish to have the line rotate, false if not. 
+	 */
+	public void setRotate(boolean rotate) {
+		this.rotate = rotate;
 	}
 	
 }
